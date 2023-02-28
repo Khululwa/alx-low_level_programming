a8ef207ee469a783317acbc2080679d4f0abea55
khululwa <kushfiglan@gmail.com>
2023-02-27 23:25:50 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#iclude "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\0');
+        {
+		_putchar(*str++);
+	}
+       _putchar('\n');
+}
