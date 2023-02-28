2a561eb7a7e858545b31e845578008b6477591f3
khululwa <kushfiglan@gmail.com>
2023-02-27 22:14:57 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,11 @@
-#include <stdio.h>
-
-void reset_to_98(int *);
-
+#include "main.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * reset_to_98 - upate the value of a var using pointers
+ * @n: int to update
+ * Return: 0
  */
-int main(void)
-{
-	int n;
 
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("%d\n", n);
-	return (0);
-}
+void reset_to_98(int *n)
+{
+	*n = 98
+}	
