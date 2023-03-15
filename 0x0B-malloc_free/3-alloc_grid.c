b69f1d24bb4390012ad8db7114c4e1d802b3ceb1
khululwa <kushfiglan@gmail.com>
2023-03-14 17:13:47 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,72 +2,45 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - nested loop to make grid
- * @width: width input
- * @height: height input
- * Return: pointer to 2 dim. array
- */
+* alloc_grid - nested loop to make grid
+* @width: width input
+* @height: height input
+* Return: pointer to 2 dim. array
+*/
+
 int **alloc_grid(int width, int height)
+
 {
 	int **mee;
 	int x, y;
 
 	if (width <= 0 || height <= 0)
 
-		return (NULL);
+	return (NULL);
 
-mee = malloc(sizeof(int *) * height);
-		
+	mee = malloc(sizeof(int *) * height);
 
-		
 	if (mee == NULL)
-		
-		return (NULL);
-		
 
-		
+	return (NULL);
+
 	for (x = 0; x < height; x++)
-		
+
 	{
-		
-		mee[x] = malloc(sizeof(int) * width);
-		
 
-		
-		if (mee[x] == NULL)
-		
-		{
-		
-			for (; x >= 0; x--)
-		
-				free(mee[x]);
-		
+	mee[x] = malloc(sizeof(int) * width);
 
-		
-			free(mee);
-		
-			return (NULL);
-		
-		}
-		
-	}
-		
+	if (mee[x] == NULL)
 
-		
-	for (x = 0; x < height; x++)
-		
 	{
-		
-		for (y = 0; y < width; y++)
-		
-			mee[x][y] = 0;
-		
-	}
-		
 
-		
-	return (mee);
-		
+	for (; x >= 0; x--)
+
+	free(mee[x]);
+	free(mee);
+
+	return (NULL);
+
+	}
+
 }
-
-
